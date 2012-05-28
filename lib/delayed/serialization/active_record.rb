496c6677a8e9803fecf1c76e0f1fae1fe2e7203d
Alexander Kellett <a@lypanov.net>
2012-05-28 18:55:36 +0200

    comment out yaml_new override

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -1,11 +1,15 @@
 class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
+=begin
+  # this blocks the use of reusing yaml for inserting AR records unavailable in current database (as in, import)
+
   def self.yaml_new(klass, tag, val)
     klass.unscoped.reset.find(val['attributes'][klass.primary_key])
   rescue ActiveRecord::RecordNotFound
     raise Delayed::DeserializationError
   end
+=end
 
   def to_yaml_properties
     ['@attributes']
